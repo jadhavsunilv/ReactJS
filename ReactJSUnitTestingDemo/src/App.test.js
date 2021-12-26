5b877a7c5866d57bf19c2c25847e102bb04321d1
sunil Jadhav <jadhavsunilv@gmail.com>
2021-12-26 23:25:04 +0530

    Add files via upload
    React js TDD approaches(unsing JEST and Enzyme)

---
+++ ReactJSUnitTestingDemo/src/App.test.js
@@ -0,0 +1,50 @@
+import App from './App';
+import { shallow } from 'enzyme';
+import { findByTestAtrr,testStore } from './../src/Utils/index';
+import React from 'react';
+
+const setUp = (initialState={}) => {
+    const store = testStore(initialState);
+    const wrapper = shallow(<App store={store} />).childAt(0).dive();
+    return wrapper;
+};
+
+describe('App Component', () => {
+
+    let wrapper;
+    beforeEach(() => {
+        const initialState = {
+            posts: [{
+                title: 'Example title 1',
+                body: 'Some text'
+            }, {
+                title: 'Example title 2',
+                body: 'Some text'
+            }, {
+                title: 'Example title 3',
+                body: 'Some text'
+            }]
+        }
+        wrapper = setUp(initialState);
+    });
+
+    it('Should render without errors', () => {
+        const component = findByTestAtrr(wrapper, 'appComponent');
+        expect(component.length).toBe(1);
+    });
+
+    it('exampleMethod_updatesState Method should update state as expected', () => {
+        const classInstance = wrapper.instance();
+        classInstance.exampleMethod_updatesState();
+        const newState = classInstance.state.hideBtn;
+        expect(newState).toBe(true);
+    });
+
+    it('exampleMethod_returnsAValue Method should return value as expected', () => {
+        const classInstance = wrapper.instance();
+        const newValue = classInstance.exampleMethod_returnsAValue(6);
+        expect(newValue).toBe(7);
+    });
+
+
+});
